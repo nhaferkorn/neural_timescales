0fc2b38503fd34b7c7438e4cd685e791682cd8b7
nhaferkorn <nele.haferkorn@web.de>
2025-10-14 00:06:09 +0200

    Revert "reading multiple bdf files & event files"
    This reverts commit bac3d5b22da92ecc1ad5a76cb0cf42d9e67750e2.

--- src/eeg2bids.py
+++ src/eeg2bids.py
@@ -1,10 +1,14 @@
 ########## Converts EDF data to BIDS format ##################
-import os
-import mne
 
-from pathlib import Path   
+# make relevant imports
+import shutil
+from pathlib import Path
+
+import mne
+import os
 
 from mne_bids import BIDSPath, print_dir_tree, write_raw_bids
+<<<<<<< HEAD
 
 from src.setup_analysis import *
 
@@ -27,39 +31,30 @@ for raw in raws:
     events.append(mne.write_events(eeg_dir / f"events_{raw}.tsv", event))
 
 print(events)
+=======
+from mne_bids.stats import count_events
+>>>>>>> parent of bac3d5b (reading multiple bdf files & event files)
 
 
-# for file in os.listdir(data_dir):
-#     filename = os.fsdecode(file)
-#     if filename.endswith(".bdf"):
-#         print(file)
-#         mne.io.read_raw_bdf(file, preload=False)
-#     else: 
-#          continue
+# Get the data 
+data_directory = "C:/Users/neleh/OneDrive - Radboud Universiteit/Documents/CNS_Master/Master_Thesis/neural_timescales/data"
+# data_dir = Path(mne.get_config("C:/Users/neleh/OneDrive - Radboud Universiteit/Documents/CNS_Master/Master_Thesis/neural_timescales/data"))
+raw = mne.io.read_raw_bdf(os.path.join(data_directory,'101.bdf'), preload=False)
 
-# raws = [mne.io.read_raw_bdf(file, preload=False) for file in os.listdir(data_dir)]
-# events = mne.find_events(raw, stim_channel="Status", initial_event=False)
+print(raw)
+events = mne.find_events(raw, stim_channel="Status", initial_event=False)
 
+#write events to tsv file (I need to adjust path)
+mne.write_events("events.tsv", events)
+# write_raw_bids() tries to extract as much meta data as possible from the raw data and then formats it in a BIDS compatible way
+subject_id = "01"
 
-# Subtract the marker offset value from all elements in the third column
-# for event in range(len(events)):
-#       events[event][2] =  events[event][2] - 64512
-# print(events)
-
-
-# # define a task name 
+# define a tasl name and a directory where to save the data to
 task = "dseeg"
 
-# add control flow to check whether this directory already consists, if not create it
-bids_root = data_dir / "data_bids"
-
-if bids_root.exists():
-    print("BIDS root directory exists")
-    
-else:
-    print("Creating BIDS root directory")
-    bids_root.mkdir()
-
+## this is not good practice - I need to change this!
+## add control flow statement - and check whether this directory already consists, if not create it
+bids_root = "C:\\Users\\neleh\\OneDrive - Radboud Universiteit\\Documents\\CNS_Master\\Master_Thesis\\neural_timescales\\data\\data_bids"
 
 # extract subject ID from file names
 
@@ -78,12 +73,16 @@ subject_ids = [1, 2, 3]
 
 
 # let's write the BIDS data
-# bids_path = BIDSPath(subject = subject_id, task = task, root=bids_root)
+bids_path = BIDSPath(subject = subject_id, task = task, root=bids_root)
 
-# # check how to add the events file to the BIDS path
-# bids_path.update(subject="01", task="dseeg", suffix="events", extension=".tsv"
-# )
-# write_raw_bids(raw, bids_path, overwrite=True)
+# check how to add the events file to the BIDS path
+bids_path.update(subject="01", task="dseeg", suffix="events", extension=".tsv"
+)
+write_raw_bids(raw, bids_path, overwrite=True)
 
-# print BIDS directory - nice, this seems to work
-# print_dir_tree(bids_root)
+# and print fresh BIDS directory - nice, this seems to work
+print_dir_tree(bids_root)
+
+# get an overview of the events on the whole dataset - this doesn't work, because events.tsv file is not created automatically from bdf file
+# counts = count_events(bids_root)
+# counts
