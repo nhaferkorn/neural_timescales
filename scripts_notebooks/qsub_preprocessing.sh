016d9dcb92fb56760c92102d6f91aec7de67b664
Nele Haferkorn <nelhaf@mentat006.dccn.nl>
2025-11-16 16:26:01 +0100

    Delete qsub sh

--- scripts_notebooks/qsub_preprocessing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# middleman script for submitting analysis to HPC cluster
-# activates right conda environment
-# runs script: input 1 is the script, input 2 is the subject, as inherited from
-# the runner file.
-
-source activate neural_timescales
-eval python $1 $2
