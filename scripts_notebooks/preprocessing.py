c2aefb923a09a9c1d0bcf939b13c9cf1602c4770
Nele Haferkorn <nelhaf@mentat006.dccn.nl>
2025-11-28 12:02:53 +0100

    minor changes

--- scripts_notebooks/preprocessing.py
+++ scripts_notebooks/preprocessing.py
@@ -5,14 +5,12 @@ import sys
 import numpy as np
 import mne
 import fooof 
-import glob
-import re
 import pickle
 
 sys.path.append('/project/4180000.57/neural_timescales/src')
 
 # import variables and paths
-from settings import PROJECT_DIR, EEG_DIR, EVENT_DICT, EVENT_DICT_CLEAN, DERIV_DIR, RAW_CLEANED
+from settings import PROJECT_DIR, EEG_DIR, EVENT_DICT, EVENT_DICT_CLEAN, DERIV_DIR, RAW_CLEANED, keys
 
 ########################################################
 ## SETUP
@@ -20,11 +18,11 @@ from settings import PROJECT_DIR, EEG_DIR, EVENT_DICT, EVENT_DICT_CLEAN, DERIV_D
 # set system variables
 param1 = sys.argv[1]
 
-# Toggle ON or OFF - To BE FIXED: they depend on one another if I dont save and load intermediate results
+# Toggle ON or OFF 
 RUN_PLOT_EVENTS = False
 RUN_ANNOTATIONS = True
 FIND_EOGs = False
-RUN_ICA = True # you only need to run this once per subject, because it then saves the ica solution
+RUN_ICA = True # Run once per subject, then ICA solution is saved
 APPLY_ICA = True 
 RUN_EPOCHS = False
 
@@ -45,8 +43,8 @@ def preprocessing(sub):
     raw = mne.set_bipolar_reference(raw, 'EXG1', 'EXG2', ch_name='HEOG')
 
     # specify line noise frequency
-    # mne.filter.notch_filter
-    raw.info['line_freq'] = 50.
+    # mne.filter.notch_filter 
+    raw.info['line_freq'] = 50
 
     # set montage directly on raw object (and not a copy of it!) > this creates a new entry in info object
     raw.set_montage("biosemi32", on_missing = "ignore")
@@ -54,33 +52,19 @@ def preprocessing(sub):
     # set average reference: 
     raw_ref = raw.copy().set_eeg_reference(ref_channels = 'average')
 
-    # filter the data (choose same filter settings as orig paper)
+    # bandpass filter the data 
     raw_ref = raw_ref.copy().filter(l_freq = 0.5, h_freq=30)
     
-    # find events
+    # find events & subtract marker offset
     events = mne.find_events(raw, stim_channel = "Status", initial_event=False, shortest_event=1)
     events[:,2] = events[:, 2] - 64512
-
-    # keys of all the events of interest (move into settings file)
-    keys = {'Start Practice Trial',
-        'Start Encoding','Encoding Stimulus Onset Baseline Left', 'Encoding Stimulus Onset Baseline Right', 'Encoding Stimulus Onset Distraction Left Target', 'Encoding Stimulus Onset Distraction Right Target'
-         ,'Fixation Onset Enc', 'Cue Onset', 
-         'Response Natural', 'Response Manmade', 'Response None Enc',
-        'Fixation Onset Enc', 'Cue Onset', 'Rest onset', 'Rest offset', 'End Encoding',
-        'Start Retrieval', 'Retrieval Stimulus Onset Baseline Left', 'Retrieval Stimulus Onset Baseline Right',
-        'Retrieval Stimulus Onset Distraction Left Target', 'Retrieval Stimulus Onset Distraction Right Target',
-        'Retrieval Stimulus Onset Distraction Right Distractor', 'Retrieval Stimulus Onset Distraction Left Distractor',
-        'Retrieval Stimulus Onset New', 'Response Old', 'Response New', 'Response None ON',
-        'Confidence Onset', 'Response Confidence 1', 'Response Confidence 2', 'Response Confidence 3', 'Response Confidence None',
-        'Fixation Onset Ret', 'End Retrieval'}
-
 
     events_of_interest = {k: EVENT_DICT[k] for k in keys}
 
     if RUN_PLOT_EVENTS:      
         fig = mne.viz.plot_events(
         events, sfreq=raw.info["sfreq"], first_samp=raw.first_samp, event_id=events_of_interest, on_missing='ignore')
-        fig.suptitle(f"Events-{sub}", fontsize=14)
+        fig.suptitle(f"Events-{sub}")
 
         fig.savefig(os.path.join(DERIV_DIR, 'Events', f'Events_Sub-{sub}.png'))
 
@@ -89,7 +73,6 @@ def preprocessing(sub):
 
         ##  Crop the signal before Practice Trial 1 and End of Retrieval
         events_mapping_blocks = mne.pick_events(events, include = [99, 93])
-        print(events_mapping_blocks)
 
         # events dict
         mapping_blocks = {99:"Start Practice Trial", 93:"End Retrieval"}
@@ -97,11 +80,11 @@ def preprocessing(sub):
         # convert events array into annotation
         annotations_mapping_blocks = mne.annotations_from_events(events, sfreq = raw_ref.info["sfreq"], event_desc = mapping_blocks)
 
-        # this should yield 3 annotations for most subjects
+        # should result 3 annotations for most subjects
         for ann in annotations_mapping_blocks:
             print(ann["onset"])
         
-        # plot those two events for sake of sanity check
+        # sanity check: plot those two events 
         raw_ref.plot(events_mapping_blocks, event_id = mapping_blocks, block = True)
 
         # crop the data 
@@ -110,11 +93,8 @@ def preprocessing(sub):
 
         print("\nNOW CREATING THE CROPPED SIGNAL\n")
         raw_crop = raw_ref.copy().crop(tmin = annotations_mapping_blocks[0]["onset"], tmax = annotations_mapping_blocks[2]["onset"])
-
-
-        ## Annotate the rest breaks (Double check if this works as intended)
         
-        # Annotate rest periods - ADD some output print messages
+        # Annotate rest periods - TO BE FIXED
         mapping_rests = {90:"Rest onset", 91:"Rest offset"}
 
         annot_from_events_rests = mne.annotations_from_events(events, event_desc = mapping_rests, sfreq = raw_crop.info["sfreq"], orig_time=raw_crop.info["meas_date"],)
@@ -127,7 +107,7 @@ def preprocessing(sub):
         onsets_rests = []
         durations_rests = []
 
-        # okay, this works; but might not be the most elegant solution
+        # okay, this works; but is definitely not the most elegant solution 
         for i in range(0, len(raw_crop.annotations.onset)-1, 2):
             
             # set onset of rest
@@ -137,7 +117,6 @@ def preprocessing(sub):
             # set duration of rest
             durations_rest = raw_crop.annotations.onset[i+1] - raw_crop.annotations.onset[i]
             durations_rests.append(durations_rest)
-
 
         print(onsets_rests)
         print(durations_rests)
@@ -152,7 +131,6 @@ def preprocessing(sub):
             description=descriptions_rest,
             orig_time=orig_times)
 
-
         # are these then saved or do they need to be reloaded?
         raw_crop.set_annotations(annot_from_events_rests + rest_annots)
 
@@ -164,7 +142,7 @@ def preprocessing(sub):
             annot_from_file = mne.read_annotations(os.path.join(DERIV_DIR, "raw_annotations", f"{sub}-annotations.fif"))
             print(annot_from_file)
 
-            # now, if we want to set new annotations in interactive plot mode
+            # annotate in interactive plot mode
             raw_crop.set_annotations(annot_from_file, emit_warning=False)
             raw_crop.plot(block=True)
 
@@ -183,7 +161,7 @@ def preprocessing(sub):
         # load saved annotations 
         annot_from_file = mne.read_annotations(os.path.join(DERIV_DIR, "raw_annotations", f"{sub}-annotations.fif"))
 
-        # # set annotations
+        # set annotations
         raw_crop.set_annotations(annot_from_file)
 
         # find eog events & plot them on top of raw data
@@ -226,12 +204,12 @@ def preprocessing(sub):
    
     if APPLY_ICA:
 
-        # but I guess it also should be possible to save the ICA solution in the previous step & load it again
+        # load subjeccts ICA solution
         ica = mne.preprocessing.read_ica(os.path.join(DERIV_DIR, 'ica', f'{sub}' + '-ica.fif'))
         
         if not os.path.isfile(os.path.join(DERIV_DIR, 'ica', f'{sub}_components')):
 
-            # ask for user input to indicate the components
+            # ask for user input to indicate the components (separated by spaces)
             components = [int(x) for x in input("ENTER INDICES OF COMPONENTS TO REMOVE: ").split()]
             
             # save subject solution as pickle object
@@ -258,7 +236,7 @@ def preprocessing(sub):
         # plot the signal with blinks removed
         raw_crop.plot(block=True)
 
-        # plot the signal after applying ICA and removal of ocular components 
+        # plot the signal after applying ICA and removal of blink components 
         reconst_raw.plot(block=True)
 
         print("\nSAVING PREPROCESSED DATA\n ")
@@ -280,7 +258,6 @@ def preprocessing(sub):
         # sanity check 
         print("INFO OBJECT OF:", reconst_raw.info)
 
-
         print(f"NOW RUNNING EPOCHS FOR SUB-{sub}\n")
 
         # input: cleaned data (after ICA and manual rejection of bad spans)
@@ -290,15 +267,10 @@ def preprocessing(sub):
         epochs_after_rejection = epochs.drop_bad()
 
         plot_dropped = epochs_after_rejection.plot_drop_log()
-
-        # adjust output directory
-        # plot_dropped.savefig(os.path.join())
-
+ 
         # compute the channel stats based on a drop_log from epochs (returns total percentage of epochs dropped)
         dropped_percent = epochs_after_rejection.drop_log_stats()
         print(dropped_percent)
-
-        # save the epochs
 
 
 if __name__ == "__main__":
