366897a18064472e79d08620beecfa54d6f4e0b7
Nele Haferkorn <nelhaf@dccn-c071.dccn.nl>
2026-03-05 14:10:05 +0100

    timefreq analysis old

--- scripts_notebooks/run_timefreq.py
+++ scripts_notebooks/run_timefreq.py
@@ -7,118 +7,95 @@ import os
 import sys
 import matplotlib.pyplot as plt
 
-
 # import variables and paths
-from timescales_memory.settings import PROJECT_DIR, EEG_DIR, EVENT_DICT, EVENT_DICT_CLEAN, DERIV_DIR
+from timescales_memory.settings import PROJECT_DIR, EEG_DIR, EVENT_DICT, DERIV_DIR, RAW_CLEANED
 
 # set system variables
 sub = sys.argv[1]
 
-
 # read cleaned data
+print(f"LOADING CLEANED DATA FOR SUB-{sub}\n")
+reconst_fname = f'sub-{sub}-raw_cleaned.fif'
+RAW_CLEANED_SUB = os.path.join(RAW_CLEANED, reconst_fname)
 
+reconst_raw = mne.io.read_raw_fif(RAW_CLEANED_SUB, preload=True)
 
+# find events
+events = mne.find_events(reconst_raw, stim_channel = "Status", initial_event=False, shortest_event=1)
+events[:,2] = events[:, 2] - 64512
+
+# run epochs 
+keys = {'Encoding Stimulus Onset Baseline Left', 'Encoding Stimulus Onset Baseline Right', 'Encoding Stimulus Onset Distraction Left Target', 'Encoding Stimulus Onset Distraction Right Target'
+          ,'Fixation Onset Enc'}
+
+events_of_interest = {k: EVENT_DICT[k] for k in keys}
+
+epochs_cleaned = mne.Epochs(reconst_raw, events = events, event_id = events_of_interest, tmin = -1.5, tmax=1.5, baseline = None, reject_by_annotation=True, picks = 'eeg', on_missing="ignore")
 
 # Power estimates pooled over distraction
-epochs_cleaned = mne.Epochs(reconst_raw_ref, events = events, event_id = events_of_interest, tmin = -1, tmax=1, baseline=None, preload=True)
-
-epochs_left_after_rejection = epochs_cleaned[['Encoding Stimulus Onset Baseline Left', 'Encoding Stimulus Onset Distraction Left Target']]
-epochs_right_after_rejection = epochs_cleaned[['Encoding Stimulus Onset Baseline Right', 'Encoding Stimulus Onset Distraction Right Target']]
-epochs_cue_onset_after_rejection = epochs_cleaned['Cue Onset']
-
+epochs_left_target = epochs_cleaned[['Encoding Stimulus Onset Baseline Left', 'Encoding Stimulus Onset Distraction Left Target']]
+epochs_right_target = epochs_cleaned[['Encoding Stimulus Onset Baseline Right', 'Encoding Stimulus Onset Distraction Right Target']]
 
 ## Compute TF power: 
 freqs = np.arange(2,30,2)
-
 n_cycles = freqs / 2
-power_left = epochs_left_after_rejection.compute_tfr(method='multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
-power_right = epochs_right_after_rejection.compute_tfr(method = 'multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
-power_cue = epochs_cue_onset_after_rejection.compute_tfr(method = 'multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
 
+power_left = epochs_left_target.compute_tfr(method='multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
+power_right = epochs_right_target.compute_tfr(method = 'multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
+
+# plotting time-freq power
 power_left.plot_joint(baseline=(-0.3, 0), mode="mean", tmin=-0.2, tmax=1, timefreqs=((0.1, 8), (.3, 8)))
-power_left.plot_joint(baseline=(-0.3, 0))
-
-
-power_left.plot_topo(baseline=(-0.3, 0), mode="mean", title=f"{sub} Average Power - Left Enc Stimuli (after ICA)")
-power_right.plot_topo(baseline=(-0.3, 0), title=f"{sub} Average Power - Right Enc Stimuli (after ICA)") # this looks extremely weird & cannot be right
-
-power_cue.plot_topo(baseline=(-0.3, 0), title=f"{sub} Average Power - Cue Onset (after ICA)") # this looks extremely weird & cannot be right
-
-## Plot Evokeds for all channels
-
-evoked_left = epochs_left_after_rejection.average().plot(titles=f"{sub} Evoked - Left Attend", picks = 'eeg')
-evoked_right = epochs_right_after_rejection.average().plot(titles=f"{sub} Evoked - Right Attend", picks = 'eeg')
-evoked_cue = epochs_cue_onset_after_rejection.average().plot(titles=f"{sub} Evoked - Cue", picks = 'eeg')
-
-times = np.linspace(0.05, 0.13, 5)
-epochs_left_after_rejection.average().plot_topomap(times=times, colorbar=True)
+power_left.plot_topo(baseline=(-0.3, 0), mode="mean", title=f"{sub} Average Power - Left Enc Stimuli")
+power_right.plot_topo(baseline=(-0.3, 0), title=f"{sub} Average Power - Right Enc Stimuli") 
 
 
 
+###################################################################################################################################################################################################
 
-## POWER MODULATION INDEX
 
-## "To identify sensors and frequencies that reliably distinguished between the attentional conditions without any contribution from encoding-related processes >> they analyzed the cue window (-750 to -250ms relative to stimulus onset)"
-## In the cue window, subjects were instructed to covertly direct their attention to the cued hemifield, but encoding related processes were expected to be minimal
+                                                                        ## Replication of Syanah's analysis
+
+
+##################################################################################################################################################################################################
 
 ## 1. Identify the sensors in which alpha power was modulated strongly in the cue-target interval:
-# - calculate difference between alpha power for attending left and attending right trials & applying cluster-based permutation test
+# calculate difference between alpha power for attending left and attending right trials & applying cluster-based permutation test
 
-# Brittas adivce: compute grand average or use get_data method and then divide on raw data
-# What is the difference between TRFAverage and Grand Average??
+print("NOW RUNNING REPLICATION ANALYSIS")
 
-# pmi_numerator = (power_left_good - power_right_good) 
-# pmi_denomenator = (power_left_good + power_right_good)
+# not sure whether I should apply baseline correction or not
+epochs_cue_window = mne.Epochs(reconst_raw, events = events, event_id = events_of_interest, tmin=-0.75, tmax = -0.25, baseline=None,  preload = True)
 
+epochs_attend_left = epochs_cue_window[['Encoding Stimulus Onset Baseline Left', 'Encoding Stimulus Onset Distraction Left Target']]
 
+epochs_attend_right = epochs_cue_window[['Encoding Stimulus Onset Baseline Right', 'Encoding Stimulus Onset Distraction Right Target']]
 
-## REPLICATION WYNN on cleaned data
+freqs = np.arange(2,30,2)
 
-# keys = {'Encoding Stimulus Onset Baseline Left', 'Encoding Stimulus Onset Baseline Right', 'Encoding Stimulus Onset Distraction Left Target', 'Encoding Stimulus Onset Distraction Right Target'
-#          ,'Fixation Onset Enc', 'Cue Onset'}
-# events_of_interest = {k: EVENT_DICT[k] for k in keys}
+n_cycles = freqs / 2
 
-# # amplitude rejection criteria
-# rejection_criteria = dict(eeg=100e-6,eog = 200e-6)
-
-
-# # print reconst_raw_ref object and info
-# print(reconst_raw_ref)
-
-
-# # extract -2000ms - 2000ms epochs
-# epochs_wynn = mne.Epochs(reconst_raw_ref, events = events, event_id = events_of_interest, tmin=-2, tmax = 2, baseline=None, reject = rejection_criteria, preload = True)
-
-# epochs_wynn_attend_left = epochs_wynn[['Encoding Stimulus Onset Baseline Left', 'Encoding Stimulus Onset Distraction Left Target']]
-
-# epochs_wynn_attend_right = epochs_wynn[['Encoding Stimulus Onset Baseline Right', 'Encoding Stimulus Onset Distraction Right Target']]
-
-
-# freqs = np.arange(2,30,2)
-
-# n_cycles = freqs / 2
-
-# ## Compute PSD average
+# Compute PSD 
 # psd_wynn_left = epochs_wynn_attend_left.compute_psd(method='multitaper', tmin = -0.75, tmax = 0.75)
 # psd_wynn_right = epochs_wynn_attend_left.compute_psd(method = 'multitaper', tmin = -0.75, tmax = 0.75)
 
 
-# ## Compute TRF
-# power_tf_wynn_left = epochs_wynn_attend_left.compute_tfr(method='multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
-# power_tf_wynn_right = epochs_wynn_attend_left.compute_tfr(method = 'multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
+# Compute TRF
+power_tf_left = epochs_attend_left.compute_tfr(method='multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
+power_tf_right = epochs_attend_left.compute_tfr(method = 'multitaper', freqs=freqs, n_cycles=n_cycles, average=True, decim=3)
 
 
-# ## Plot PDS and TRF
-# psd_wynn_left.plot()
-
-# # I need to specify channels for this!!
-# power_tf_wynn_left.plot()
-
-# ## how can I pool an average trf object over channels - doesn't exist, but check shape
-# ## What does it mean that AverageTRF object is not subscriptable; then how can I index the power of a single channel??
+# Compute TRF Difference (Left - Right)
+power_diff = power_tf_left - power_tf_right
 
 
-# ### Grand Averages - mne.grand_averages()
-# ### make grand average of a list of Evoked, Average TRF or Spectrum data
+print(type(power_diff))
 
-# ## use for mean of all subjects or runs
+# plot power_diff
+power_diff.plot_topo(baseline=(-0.3, 0), mode="mean", title=f"{sub} Average Power - Left Enc Stimuli")
+
+
+
+
+# Brittas adivce: compute grand average or use get_data method and then divide on raw data
+# pmi_numerator = (power_left_good - power_right_good) 
+# pmi_denomenator = (power_left_good + power_right_good)
