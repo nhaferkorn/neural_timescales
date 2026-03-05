2e34d243b92b023afb46c4a32b5ae48a1bf3cc0d
Nele Haferkorn <nelhaf@dccn-c072.dccn.nl>
2026-03-05 13:25:26 +0100

    add simulation script

--- scripts_notebooks/run_simulations.py
+++ scripts_notebooks/run_simulations.py
@@ -17,6 +17,8 @@ from statsmodels.tsa.stattools import acf as compute_acf
 from neurodsp.spectral import compute_spectrum
 from neurodsp.spectral import compute_spectrum, trim_spectrum
 
+# for animating it: https://matplotlib.org/stable/gallery/widgets/slider_demo.html
+
 # specify signal parameters
 n_seconds = 1
 fs = 300
@@ -32,4 +34,3 @@ times_vector = np.arange(0,1,1/fs) # TODO: check how it works exactly
 times = neurodsp.utils.create_times(n_seconds, fs)
 print(times)
 
-# for animating it: https://matplotlib.org/stable/gallery/widgets/slider_demo.html
